8a4abc163eba11fdb8a731ded1aa93ab163c1368
Sam <sam.soghomonyan@bk.ru>
2025-05-02 16:22:57 +0400

    to-do

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Todo from "@/components/todo";
 import dotenv from 'dotenv';
 dotenv.config();
-const theme = process.env.SECOND_THEME
+const theme = process.env.FIRST_THEME
 console.log(theme)
 export default function Home(){
   return <Todo theme={theme as string } />
